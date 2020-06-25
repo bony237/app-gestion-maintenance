d4c886d25176063a84ef41d65926eecc0c8f93d3
bony237 <nbj.ngoube@gmail.com>
2020-06-25 03:05:28 +0100

    django html template - part1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /*Calcul de fiabilité, disponibilité et MTBF */
+
+/*Switch div_infos between them*/
 const switchBtnAll = document.querySelectorAll('.switchBtn');
 
 
@@ -14,3 +16,17 @@ switchBtnAll.forEach((switchBtn) => {
         });
     });
 });
+
+/*add class active to the first link of machines */
+
+const firstLink = document.querySelector('a[role = "tab"]');
+
+if (firstLink != null) {
+    firstLink.classList.add('active');
+
+    /*add class show and active to the first div infos of machines */
+
+    document.querySelector('div[role = "tabpanel"]').classList.add('show', 'active');   
+}
+
+
